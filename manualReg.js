ee9c415864f1018deacc3af8b20545f896791613
Will <vclwei@qq.com>
2025-01-15 17:13:25 +0800

    Feat: Add manual register mode & add proxy for register/login/createProfile/

---
+++ manualReg.js
@@ -0,0 +1,87 @@
+import { readFile, saveToFile } from './utils/helper.js';
+import log from './utils/logger.js'
+import banner from './utils/banner.js';
+import readline from 'readline/promises';
+import {
+    registerUser,
+    loginUser,
+    createUserProfile,
+    confirmUserReff,
+    getIp
+} from './utils/api.js'
+
+// Read register info from register.csv [email,password,proxy]
+// Save result to fullinfo.txt [email,password,proxy,token]
+const main = async () => {
+    log.info(banner);
+
+    const accounts = await readFile("register.csv");
+    if (accounts.length === 0) {
+        log.error('No accounts found in register.csv');
+        return;
+    }
+
+    const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+    for (const [index, account] of accounts.entries()) {
+        try {
+            const [email, password, proxy] = account.split(',');
+            log.info(`Processing account ${index + 1}: ${email} ${proxy}`);
+
+            const regResponse = await registerUser(email, password, proxy);
+            let token = null;
+            if (regResponse?.error === 'email already registered') {
+                const loginResp = await loginUser(email, password, proxy);
+                if (loginResp?.data?.has_entered_referral_code) {
+                    log.info(`Account ${email} already has a referral code`);
+                    token = loginResp.data.token;
+                    await saveToFile("fullinfo.txt", `${email},${password},${proxy},${token}`);
+                    continue
+                }
+
+                log.error(`Account ${email} already registered but not has a referral code`);
+                token = loginResp.data.token;
+            }
+            else if (!regResponse?.data?.token) {
+                log.error(`Failed to register account ${email}`);
+                continue;
+            }
+            else {
+                token = regResponse.data.token;
+
+                log.info(`Trying to create profile for ${email}`);
+                await createUserProfile(token, { step: 'username', username: email.split('@')[0] }, proxy);
+                await createUserProfile(token, { step: 'description', description: "AI Startup" }, proxy);
+            }
+
+            let isEntered = false;
+            while (!isEntered) {
+                const reffCode = await rl.question('Enter Your Referral code or (CTRL + C to exit): ');
+                log.info(`Use Referral code: ${reffCode}`);
+                const confirm = await confirmUserReff(token, reffCode);
+                if (confirm?.data?.token) {
+                    log.info('Referral confirmed successfully');
+                    saveToFile("tokens.txt", confirm.data.token);
+                    saveToFile("proxy.txt", proxy);
+                    saveToFile("fullinfo.txt", `${email},${password},${proxy},${confirm.data.token}`)
+                    isEntered = true;
+                }
+            }
+        } catch (err) {
+            log.error('Error creating account:', err.message);
+        }
+    }
+};
+
+// Handle CTRL+C (SIGINT)
+process.on('SIGINT', () => {
+    log.info('SIGINT received. Exiting...');
+    process.exit();
+});
+
+// Handle uncaught exceptions
+process.on('uncaughtException', (err) => {
+    log.error('Uncaught exception:', err);
+    process.exit(1);
+});
+
+main();
--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,6 @@
 import randomUseragent from 'random-useragent';
 import axios from 'axios';
+import axiosRetry from 'axios-retry'
 import log from './logger.js';
 import {
     newAgent
@@ -7,12 +8,40 @@ import {
 
 const userAgent = randomUseragent.getRandom();
 const headers = {
-    "Accept": "application/json",
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
     "Accept-Language": "en-US,en;q=0.9",
-    "User-Agent": userAgent,
+    "Origin": "https://app.depined.org",
+    "Referer": "https://app.depined.org/",
+    "Priority": "u=1, i",
+    "Sec-Ch-Ua": '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": '"macOS"',
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-site",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
 }
 
-export const registerUser = async (email, password) => {
+axiosRetry(axios, {
+    retries: 3, // 设置重试次数
+    retryDelay: axiosRetry.exponentialDelay, // 设置重试延迟策略
+});
+
+export const getIp = async (proxy) => {
+    log.info(`Getting IP from ${proxy}`);
+    const agent = newAgent(proxy);
+    log.info(JSON.stringify(agent, null, 2));
+    const response = await axios.get('http://ip-api.com/json/', {
+        httpsAgent: agent,
+        httpAgent: agent
+    });
+    log.info(response.data);
+    return response.data.ip;
+}
+
+export const registerUser = async (email, password, proxy) => {
+    const agent = newAgent(proxy);
     const url = 'https://api.depined.org/api/user/register';
 
     try {
@@ -20,17 +49,24 @@ export const registerUser = async (email, password) => {
             headers: {
                 ...headers,
                 'Content-Type': 'application/json'
-            }
+            },
+            httpsAgent: agent,
+            httpAgent: agent
         });
+        log.info(JSON.stringify(response, null, 2));
         log.info('User registered successfully:', response.data.message);
         return response.data;
     } catch (error) {
         log.error('Error registering user:', error.response ? error.response.data : error.message);
+        if (error.response) {
+            return error.response.data;
+        }
         return null;
     }
 };
 
-export const loginUser = async (email, password) => {
+export const loginUser = async (email, password, proxy) => {
+    const agent = newAgent(proxy);
     const url = 'https://api.depined.org/api/user/login';
 
     try {
@@ -38,9 +74,11 @@ export const loginUser = async (email, password) => {
             headers: {
                 ...headers,
                 'Content-Type': 'application/json'
-            }
+            },
+            httpsAgent: agent,
+            httpAgent: agent
         });
-        log.info('User Login successfully:', response.data.message);
+        log.info('User Login successfully:', response.data);
         return response.data;
     } catch (error) {
         log.error('Error Login user:', error.response ? error.response.data : error.message);
@@ -48,7 +86,8 @@ export const loginUser = async (email, password) => {
     }
 };
 
-export const createUserProfile = async (token, payload) => {
+export const createUserProfile = async (token, payload, proxy) => {
+    const agent = newAgent(proxy);
     const url = 'https://api.depined.org/api/user/profile-creation';
 
     try {
@@ -57,7 +96,9 @@ export const createUserProfile = async (token, payload) => {
                 ...headers,
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${token}`
-            }
+            },
+            httpsAgent: agent,
+            httpAgent: agent
         });
         log.info('Profile created successfully:', payload);
         return response.data;
@@ -67,7 +108,8 @@ export const createUserProfile = async (token, payload) => {
     }
 };
 
-export const confirmUserReff = async (token, referral_code) => {
+export const confirmUserReff = async (token, referral_code, proxy) => {
+    const agent = newAgent(proxy);
     const url = 'https://api.depined.org/api/access-code/referal';
 
     try {
@@ -76,7 +118,9 @@ export const confirmUserReff = async (token, referral_code) => {
                 ...headers,
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${token}`
-            }
+            },
+            httpsAgent: agent,
+            httpAgent: agent
         });
         log.info('Confirm User referral successfully:', response.data.message);
         return response.data;
